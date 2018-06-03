160ff5494bdaccd2706315db718adcddab76185c
Bruno Lima <bg.lima@hotmail.com>
2018-06-03 18:58:53 -0300

    Generalised process to module n. Added me and brother video

--- src/test_images_cv.py
+++ src/test_images_cv.py
@@ -23,48 +23,79 @@ def load_images_from_folder(folder):
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-bruno_image = face_recognition.load_image_file("../media/train/bruno_lima/bruno_01.png")
+bruno_image = face_recognition.load_image_file("../media/train/bruno_lima/bruno_05.png")
 bruno_face_encoding = face_recognition.face_encodings(bruno_image)[0]
+
+joao_image = face_recognition.load_image_file("../media/train/joao_victor/joao_01.jpg")
+joao_face_encoding = face_recognition.face_encodings(joao_image)[0]
+
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    bruno_face_encoding
+    bruno_face_encoding,
+    joao_face_encoding
 ]
 known_face_names = [
-    "Bruno Lima"
+    "Bruno Lima",
+    "Joao Victor"
 ]
 
-images = load_images_from_folder('../media/test')
-image_index = -1
+video_capture = cv2.VideoCapture('../media/test/bruno_e_joao.mp4')
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+process_each_n = 3
+frame_index = -1
 
 while True:
     # Grab a single frame of video
-    #ret, frame = video_capture.read()
-    image_index += 1
-    if ( image_index ) >= len(images):
+    ret, frame = video_capture.read()
+    if frame is None:
         break
-    frame = images[image_index]
+
+    frame_index += 1
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_frame = frame[:, :, ::-1]
+    rgb_small_frame = small_frame[:, :, ::-1]
 
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Find all the faces and face encodings in the current frame of video
+        start = time()
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-    # Find all the faces and face enqcodings in the frame of video
-    start = time()
-    face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
 
-    # Loop through each face in this frame of video
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            # If a match was found in known_face_encodings, just use the first one.
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_names[first_match_index]
 
-        name = "Unknown"
+            face_names.append(name)
 
-        # If a match was found in known_face_encodings, just use the first one.
-        if True in matches:
-            first_match_index = matches.index(True)
-            name = known_face_names[first_match_index]
+        end = time()
+        print('Detection took {} seconds...'.format(round(end - start, 2)))
+
+    process_this_frame = not (frame_index % process_each_n)
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
@@ -74,13 +105,11 @@ while True:
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    end = time()
-    print('Detection took {} seconds...'.format(round(end - start, 2)))
     # Display the resulting image
     cv2.imshow('Video', frame)
 
     # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey() & 0xFF == ord('q'):
+    if cv2.waitKey(5) & 0xFF == ord('q'):
         break
 
 # Release handle to the webcam
